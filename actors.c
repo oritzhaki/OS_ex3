417a6f0cc0e45943f0fe2c266ceeaeae19323f1d
oritzhaki <117988679+oritzhaki@users.noreply.github.com>
2023-06-04 21:16:57 +0300

    Update actors.c

--- actors.c
+++ actors.c
@@ -8,7 +8,7 @@ void* produce(void* arg) {
     int newsCount = 0;
     int weatherCount = 0;
     for (int i = 0; i < producer->numMsg; i++) {
-        //usleep(50000);
+        usleep(50000);
         char message[100];
         // Generate a random number between 1 and 3
         int randomNumber = rand() % 3 + 1;
@@ -131,7 +131,7 @@ void* printer(void* arg) {
             free(message);
         } else {
             doneCount++;
-            free(message);
+            //free(message);
             if (doneCount == 3) { // after three "Done"'s, exit
                 char* doneMessage = "DONE";
                 printf("%s\n", doneMessage);
