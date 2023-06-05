7647fc597760cf828775e1be7bee80a67c85015e
oritzhaki <117988679+oritzhaki@users.noreply.github.com>
2023-06-06 00:34:55 +0300

    Update buffers.c

--- buffers.c
+++ buffers.c
@@ -24,14 +24,6 @@ void initBoundedBuffer(BoundedBuffer* buffer, int size) {
 //    printf("size is %d\n", size);
 }
 
-// initializes unbounded queue
-void initUnboundedBuffer(UnboundedBuffer* buffer) {
-    buffer->head = NULL; //empty linked list
-    buffer->tail = NULL;
-    sem_init(&buffer->mutex, 0, 1);
-    sem_init(&buffer->fullSlots, 0, 0);
-}
-
 // pushes an element into the end of the queue (FIFO) if there is room
 void pushBoundedBuffer(BoundedBuffer* buffer, char* s) {
     sem_wait(&buffer->emptySlots); //ensures that the consumer waits if the buffer is full.
@@ -42,6 +34,44 @@ void pushBoundedBuffer(BoundedBuffer* buffer, char* s) {
 
     sem_post(&buffer->mutex);
     sem_post(&buffer->fullSlots);
+}
+
+// pops an element from the front of the queue (FIFO) if not empty
+char* popBoundedBuffer(BoundedBuffer* buffer) {
+    sem_wait(&buffer->fullSlots); //ensures that the consumer waits if the buffer is empty.
+    sem_wait(&buffer->mutex); //protect critical sections of the code
+
+    char* item = buffer->buffer[buffer->front]; // get the item in the front of the buffer - FIFO
+    buffer->front = (buffer->front + 1) % buffer->size;
+
+    sem_post(&buffer->mutex);
+    sem_post(&buffer->emptySlots);
+    return item;
+}
+
+// Destroys semaphores an frees memory
+void destroyBoundedBuffer(BoundedBuffer* buffer) {
+    sem_destroy(&buffer->mutex);
+    sem_destroy(&buffer->fullSlots);
+    sem_destroy(&buffer->emptySlots);
+    free(buffer->buffer);
+}
+
+// checks if the bounded queue is empty
+int isBoundedBufferEmpty(BoundedBuffer* buffer) {
+    int isEmpty;
+    sem_wait(&buffer->mutex); //protect critical sections of the code
+    isEmpty = (buffer->front == buffer->rear);
+    sem_post(&buffer->mutex);
+    return isEmpty;
+}
+
+// initializes unbounded queue
+void initUnboundedBuffer(UnboundedBuffer* buffer) {
+    buffer->head = NULL; //empty linked list
+    buffer->tail = NULL;
+    sem_init(&buffer->mutex, 0, 1);
+    sem_init(&buffer->fullSlots, 0, 0);
 }
 
 // pushes an element into the end of the unbounded queue (FIFO)
@@ -64,19 +94,6 @@ void pushUnboundedBuffer(UnboundedBuffer* buffer, char* s) {
     sem_post(&buffer->mutex);
 }
 
-// pops an element from the front of the bounded queue (FIFO) if not empty
-char* popBoundedBuffer(BoundedBuffer* buffer) {
-    sem_wait(&buffer->fullSlots); //ensures that the consumer waits if the buffer is empty.
-    sem_wait(&buffer->mutex); //protect critical sections of the code
-
-    char* item = buffer->buffer[buffer->front]; // get the item in the front of the buffer - FIFO
-    buffer->front = (buffer->front + 1) % buffer->size;
-
-    sem_post(&buffer->mutex);
-    sem_post(&buffer->emptySlots);
-    return item;
-}
-
 // pops an element from the start of the unbounded queue (FIFO)
 char* popUnboundedBuffer(UnboundedBuffer* buffer) {
     sem_wait(&buffer->fullSlots); //ensures that the consumer waits if the buffer is empty.
@@ -94,29 +111,12 @@ char* popUnboundedBuffer(UnboundedBuffer* buffer) {
     return item;
 }
 
-// checks if the bounded queue is empty
-int isBoundedBufferEmpty(BoundedBuffer* buffer) {
-    int isEmpty;
-    sem_wait(&buffer->mutex); //protect critical sections of the code
-    isEmpty = (buffer->front == buffer->rear);
-    sem_post(&buffer->mutex);
-    return isEmpty;
-}
-
 //checks if unbounded buffer is empty
 bool isUnboundedBufferEmpty(UnboundedBuffer* buffer) {
     sem_wait(&buffer->mutex); //protect critical sections of the code
     bool empty = (buffer->head == NULL);
     sem_post(&buffer->mutex);
     return empty;
-}
-
-// Destroys semaphores an frees memory
-void destroyBoundedBuffer(BoundedBuffer* buffer) {
-    sem_destroy(&buffer->mutex);
-    sem_destroy(&buffer->fullSlots);
-    sem_destroy(&buffer->emptySlots);
-    free(buffer->buffer);
 }
 
 //destroys semaphores and frees memory
