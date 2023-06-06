c5e57379dbbf7af0f6603003bd9c68d43dcfc728
oritzhaki <117988679+oritzhaki@users.noreply.github.com>
2023-06-06 09:57:18 +0300

    Update buffers.h

--- buffers.h
+++ buffers.h
@@ -1,13 +1,18 @@
 #ifndef BUFFERS_H
 #define BUFFERS_H
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <semaphore.h>
 #include <stdbool.h>
 #include <pthread.h>
 
+
+typedef struct Node { // for linked list
+    char* data;
+    struct Node* next;
+} Node;
 
 // bounded queue is an array with head, tail, size and personal semaphores to control traffic
 typedef struct {
@@ -20,11 +25,6 @@ typedef struct {
     sem_t emptySlots;
 } BoundedBuffer;
 
-typedef struct Node { // for linked list
-    char* data;
-    struct Node* next;
-} Node;
-
 // unbounded queue is a linked list with head, tail and personal semaphores to control traffic
 typedef struct {
     Node* head;
@@ -33,15 +33,25 @@ typedef struct {
     sem_t fullSlots;
 } UnboundedBuffer;
 
+//the queue actions:
 void initBoundedBuffer(BoundedBuffer* buffer, int size);
+
 void pushBoundedBuffer(BoundedBuffer* buffer, char* s);
+
 char* popBoundedBuffer(BoundedBuffer* buffer);
+
 void destroyBoundedBuffer(BoundedBuffer* buffer);
+
 int isBoundedBufferEmpty(BoundedBuffer* buffer);
+
 void initUnboundedBuffer(UnboundedBuffer* buffer);
+
 void pushUnboundedBuffer(UnboundedBuffer* buffer, char* s);
+
 char* popUnboundedBuffer(UnboundedBuffer* buffer);
+
 bool isUnboundedBufferEmpty(UnboundedBuffer* buffer);
+
 void destroyUnboundedBuffer(UnboundedBuffer* buffer);
 
 #endif  // BUFFERS_H
